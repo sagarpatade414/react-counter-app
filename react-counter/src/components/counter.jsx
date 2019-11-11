c00de14502a2a0d94fcaf18eaf47bd7c8b10aec6
Sagar Patade <sagarpatade414@gmail.com>
2019-11-11 15:26:25 -0500

    state rendering
    state rendering

--- react-counter/src/components/counter.jsx
+++ react-counter/src/components/counter.jsx
@@ -3,26 +3,32 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        count: 0
     };
 
-    renderTags(){
-        if(this.state.tags.length === 0) {
-            return <p>There are no tags </p>; 
-        } else {
-            return <ul> {this.state.tags.map( (item) => <li key={item}> { item }</li> )} </ul>;
-        }
-    }
+   handleIncrement= () => {
+      this.setState({ count : this.state.count + 1});
+    };
+
     render() { 
         return (
          <div>
-             {this.renderTags()}
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
     }
 
-    
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? <h1>Zero</h1> : count;
+    }
 
 
 }
