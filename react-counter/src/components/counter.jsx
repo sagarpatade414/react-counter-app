c8ab6141bd23e02f2e5a5c7fbadea9c493bc9013
Sagar Patade <sagarpatade414@gmail.com>
2019-11-11 22:01:37 -0500

    Passing props and Counters.jsx
    Passing props and Counters.jsx

--- react-counter/src/components/counter.jsx
+++ react-counter/src/components/counter.jsx
@@ -3,16 +3,18 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0
+        value: this.props.value
     };
 
    handleIncrement= () => {
-      this.setState({ count : this.state.count + 1});
+      this.setState({ value : this.state.count + 1});
     };
 
     render() { 
+        
         return (
          <div>
+             {this.props.children}
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         </div>
@@ -21,13 +23,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
-        return count === 0 ? <h1>Zero</h1> : count;
+        const { value } = this.state;
+        return value === 0 ? <h1>Zero</h1> : value;
     }
 
 
